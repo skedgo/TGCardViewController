6fd56e1d4b010ea4511a62149aada842533d8229
pnuts2 <brian@skedgo.com>
2017-03-14 11:56:02 +1100

    top level class for corner view

---
+++ TGCardViewController/TGCornerView.swift
@@ -0,0 +1,22 @@
+//
+//  TGCornerView.swift
+//  TGCardViewController
+//
+//  Created by Kuan Lun Huang on 14/3/17.
+//  Copyright © 2017 SkedGo Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class TGCornerView: UIView {
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    
+    let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: [.topLeft, .topRight], cornerRadii: CGSize(width: 10, height: 10))
+    let mask = CAShapeLayer()
+    mask.path = path.cgPath
+    layer.mask = mask
+  }
+
+}
